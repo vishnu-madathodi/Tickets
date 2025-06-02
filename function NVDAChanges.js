6dac446283ff5e967137568c3c8780649684515c
MR POOL <71345322+vishnu-madathodi@users.noreply.github.com>
2025-06-02 22:44:02 +0530

    Add files via upload

---
+++ function NVDAChanges.js
@@ -0,0 +1,40 @@
+function NVDAChanges(id){
+    try {
+        
+            var comboBox = $("." + id + "-dropbtn");
+            comboBox.find("span").attr("aria-hidden", "true");
+            comboBox.attr({
+                "role": "button",
+                "aria-haspopup": "listbox"
+            });
+            var listBox = $("#" + id + "-wrapper");
+            listBox.attr({
+                "role": "listbox",
+                "aria-hidden": "true"
+            });
+        
+            listBox.find("li").each(function(){
+                let isSelected = $(this).data("isselected") ? "true" : "false";
+                $(this).attr({
+                    "role": "option",
+                    "aria-selected" : isSelected
+                });
+                $(this).find("a").attr({
+                    "role": "option",
+                });
+                let optionTxt = $(this).find("a h5");
+                optionTxt.attr({
+                    "aria-label": optionTxt.text().trim(),
+                    "role": "none",
+                });
+            });
+            listBox.find("a.default-option").attr({"role": "option"});
+            listBox.find("a.default-option h5").attr({
+                "aria-label": "Select",
+                "role": "none"
+            });
+        
+    } catch(error) {
+        console.error(Error in the function NVDAChanges: ${error});
+    }
+}
